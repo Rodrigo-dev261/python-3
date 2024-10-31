978a39a6f6cc1bed42125c7c31697546feaf35c5
Rodrigo F. Silva <103850462+Rodrigo-dev261@users.noreply.github.com>
2024-10-31 00:41:44 -0300

    up date

---
+++ aula_1/desafio_02.py
@@ -0,0 +1,6 @@
+
+
+dia = input('Em que dia voce nasceu? ')
+mes = input('em que mes voce nasceu? ')
+ano = input('Em que ano voce nasceu? ')
+print('Vone nasceu no dia {0} de {1} de {2}. correto? '.format(dia, mes, ano))
