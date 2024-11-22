01f07ccb691bea01bea6c2cd1971b99121265b54
Rodrigo-dev261 <103850462+Rodrigo-dev261@users.noreply.github.com>
2024-11-22 14:13:29 -0300

    update
    novas atualizações

---
+++ projetos/calculadora.py
@@ -0,0 +1,24 @@
+def adicionar(x, y):
+    return x + y
+
+def subtrair(x, y):
+    return x - y
+
+def multiplicar(x, y):
+    return x * y
+
+def dividir(x, y):
+    if y != o:
+        return x / y
+    else:
+        return 'Erro: Divisão por zero!'
+    
+print('Selecione a operação: ')
+print('1. Adição')
+print('2. Subtração')
+print('3. Multiplicação')
+print('4. Divisão')
+
+escolha = input('Digite a escolha (1/2/3/4): ')
+
+num1 = 
